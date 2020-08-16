36b11b5e2b127598b817853e4df042bf13903bbe
潘博 <1350210100@qq.com>
2020-08-16 15:02:42 +0800

    chore: use iconfont

---
+++ src/components/base/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+	<svg class="icon" aria-hidden="true">
+		<use :xlink:href="'#icon-' + type"></use>
+	</svg>
+</template>
+
+<script>
+export default {
+	name: 'Icon',
+	props: { type: String }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+	display: flex;
+	overflow: hidden;
+	width: 1em;
+	height: 1em;
+	color: inherit;
+	vertical-align: -0.15em;
+	font-size: inherit;
+
+	fill: currentColor;
+}
+</style>
